199d9cf756e092b9436ad368ee3acf2820146a60
mbabat <mbabat@users.noreply.github.com>
2020-04-04 17:01:42 +0300

    Update sayac.py
    Bir kelim içerisinde sessli ve sessiz harf bulan modül. OOP kurallarına uyularak yazıldı.

--- sayac.py
+++ sayac.py
@@ -3,3 +3,44 @@
 print("Sayaç version = 1.2")
 print("##################################\n" + "#    Sesli Harf Bulma Projesi    #\n" + "#          Yazar: Babat "
       "  #\n" + "##################################", sep="")
+
+
+class HarfSayaci:
+
+    @staticmethod
+    def kelime_sor():
+        return input(str("Bir kelime giriniz: "))
+
+    def __init__(self):
+        self.sesli_harfler = "AEIİOÖUÜaeıioöuü"
+        self.sessiz_harfler = "BCÇDFGĞHJKLMNPRSŞTVYZbcçdfgğhjklmnprsştvyz"
+        self.sayac_sesli = 0
+        self.sayac_sessiz = 0
+
+    def seslidir(self, harf):
+        return harf in self.sesli_harfler
+
+    def sessizdir(self,harf):
+        return harf in self.sessiz_harfler
+
+    def artir(self):
+        for harf in self.kelime:
+            if self.seslidir(harf):
+                self.sayac_sesli += 1
+            if self.sessizdir(harf):
+                self.sayac_sessiz += 1
+        return self.sayac_sesli, self.sayac_sessiz
+
+    def ekrana_bas(self):
+        mesaj = "{} kelimesinde {} sesli harf, {} sessiz harf vardır."
+        sesli, sessiz = self.artir()
+        print(mesaj.format(self.kelime, sesli, sessiz))
+
+    def calistir(self):
+        self.kelime = self.kelime_sor()
+        self.ekrana_bas()
+
+
+if __name__ == '__main__':
+    sayac = HarfSayaci()
+    sayac.calistir()
